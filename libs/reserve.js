e3de6340b78a18530b9a97270040d047e574c52d
Kyle Lotilla <kyle_lotilla@dlsu.edu.ph>
2019-08-20 23:10:03 +0800

    -Installing Moment

--- libs/reserve.js
+++ libs/reserve.js
@@ -10,15 +10,7 @@ function setUpReserve(spaceID, locationName) {
 
 function reserveSpace (spaceID, locationName) {
 	var date = $("#datePicker").datetimepicker("viewDate");
-	date = date._d;
-	date = date.toLocaleString("en-US", {hour12: false}).split(",");
-	var mdy = date[0].split("/");
-	var year = mdy[2];
-	var month = mdy[0];
-	var day = mdy[1];
-	var formattedDate = new Date(year + "-" + month + "-" + day + "T00:00:00Z");
-
-	var reservation = {spaceID: spaceID, date: formattedDate, locationName: locationName};
+	var reservation = {spaceID: spaceID, date: date, locationName: locationName};
 	
 	$.ajax({
 		type: "POST",
@@ -26,7 +18,7 @@ function reserveSpace (spaceID, locationName) {
 		data: JSON.stringify(reservation),
 		contentType: "application/json",
 		success: function () {
-			location.replace("http://localhost:9090/reservationCreated/");
+			//location.replace("http://localhost:9090/reservationCreated/");
 		}
 	});
 }
