a05656f2bafa35c79fb4a797f270eb5f806f59e6
Kyle Lotilla <kyle_lotilla@dlsu.edu.ph>
2019-08-22 12:24:30 +0800

    -Added Parallax Background -Fixed Space Details Design

--- routes/reservationCreated.js
+++ routes/reservationCreated.js
@@ -1,7 +1,11 @@
 module.exports = function (app) {
     app.get("/reservationCreated", function (req, res) {
-        res.render("reservationCreated", {reservation: req.session.reservation, locationName: req.session.locationName, accountID: req.cookies.accountID});
-        req.session.reservation = null;
-        req.session.locationName = null;
+        if (req.cookies.accountID == "")
+            res.redirect("/home");
+        else {
+            res.render("reservationCreated", {reservation: req.session.reservation, locationName: req.session.locationName, accountID: req.cookies.accountID});
+            req.session.reservation = null;
+            req.session.locationName = null;
+        }
     });
 }
